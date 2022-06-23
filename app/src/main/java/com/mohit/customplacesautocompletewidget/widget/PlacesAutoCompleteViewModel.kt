68d31827a7c8719ad12be0c51064d0a0517dd3c4
ti-mohitsoni <75679565+mohitsintellify@users.noreply.github.com>
2022-06-23 18:01:38 +0530

    version 1.0

---
+++ app/src/main/java/com/mohit/customplacesautocompletewidget/widget/PlacesAutoCompleteViewModel.kt
@@ -0,0 +1,7 @@
+package com.mohit.customplacesautocompletewidget.widget
+
+import androidx.lifecycle.ViewModel
+
+class PlacesAutoCompleteViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
